de24e45ea00669927060d5e5cf4170e8e35e501f
Syedabdd <abdullahsyed574@gmail.com>
2026-08-04 11:18:55 +0530

    change favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -103,11 +103,10 @@ export const metadata: Metadata = {
 
   icons: {
     icon: [
-      { url: "/icon.png", type: "image/png", sizes: "192x192" },
-      { url: "/favicon.svg", type: "image/svg+xml" },
+      { url: "/Logo2.png", type: "image/png" },
     ],
     apple: [
-      { url: "/icon.png", type: "image/png", sizes: "192x192" },
+      { url: "/Logo2.png", type: "image/png" },
     ],
   },
 };
